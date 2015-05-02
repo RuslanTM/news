13fcc9b93e77fd8805733a46743269433db039c2
RuslanTM <rustemirgali@gmail.com>
2015-05-02 11:04:57 +0600

    Добавлена форма добавления новости

--- views/index.php
+++ views/index.php
@@ -7,9 +7,13 @@
 <BODY>
 <h1 align="center">Новости дня</h1>
 <?php foreach($articles as $article): ?>
-    <a href = "/article.php?id=<?php echo $article['id']; ?>"
-<h2><?php echo $article['name']; ?></h2> </a>
+    <a href =
+       "www/article.php?id=<?php echo $article['id']; ?>"
+        >
+<h2><?php echo $article['name']; ?></h2>
+    </a>
     <p><?php echo $article['content']; ?></p>
 <?php endforeach; ?>
+<a href = "/add.php">Добавить новость</a>
 </BODY>
 </HTML>
